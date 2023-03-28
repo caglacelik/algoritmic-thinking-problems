858e0f0c67b35895c16689a728d1b13ca8742ab5
caglacelik <caglacelikkk@hotmail.com>
2023-03-28 23:56:17 +0000

    fix target

--- spelling-check/target/debug/deps/spelling_check-200d93784fc33d58.d
+++
@@ -1,5 +0,0 @@
-/workspaces/algoritmic-thinking-problems/spelling-check/target/debug/deps/spelling_check-200d93784fc33d58: src/main.rs
-
-/workspaces/algoritmic-thinking-problems/spelling-check/target/debug/deps/spelling_check-200d93784fc33d58.d: src/main.rs
-
-src/main.rs:
--- spelling-check/target/debug/spelling-check.d
+++
@@ -1,1 +0,0 @@
-/workspaces/algoritmic-thinking-problems/spelling-check/target/debug/spelling-check: /workspaces/algoritmic-thinking-problems/spelling-check/src/main.rs
